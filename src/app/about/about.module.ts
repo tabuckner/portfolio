ec5794bfbceff47237c80781063d3e425519b529
Taylor Buckner <tbuckner@theappraisallane.com>
2018-08-29 09:03:19 -0500

    Added about-hero component.

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
+import { AboutHeroComponent } from './about-hero/about-hero.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AboutComponent],
+  declarations: [AboutComponent, AboutHeroComponent],
   exports: [AboutComponent]
 })
 export class AboutModule { }
