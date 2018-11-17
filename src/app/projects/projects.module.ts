dccc2b0e523e439fe5118b33be491f1a5a7688dd
Taylor Buckner <tbuckner@theappraisallane.com>
2018-11-17 10:52:13 -0600

    Basic shell component for Project Item and Project List.

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProjectsComponent],
+  declarations: [ProjectsComponent, ProjectItemComponent, ProjectListComponent],
   exports: [ProjectsComponent]
 })
 export class ProjectsModule { }
