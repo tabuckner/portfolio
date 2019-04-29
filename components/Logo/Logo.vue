d23daec9d7be09ddcaef2d7123811fbfb8089f0a
Taylor Buckner <tbuckner@theappraisallane.com>
2019-04-29 15:36:38 -0500

    Added SCSS support.

--- components/Logo/Logo.vue
+++ components/Logo/Logo.vue
@@ -1,5 +1,5 @@
 <template src="./template.html"></template>
-<style src="./styles.css"></style>
+<style src="./styles.scss" lang="scss"></style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
--- components/VuetifyLogo/VuetifyLogo.vue
+++ components/VuetifyLogo/VuetifyLogo.vue
@@ -1,6 +1,6 @@
 <template src="./template.html"></template>
 
-<style src="./styles.css"></style>
+<style src="./styles.scss" lang="scss"></style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
