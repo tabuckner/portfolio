ae0799c9877b67f8efb29c2dad0fc88311392f55
Taylor Buckner <tbuckner@theappraisallane.com>
2019-04-30 09:02:20 -0500

    Added a 404 redirect to the home route.

---
+++ pages/_.vue
@@ -0,0 +1,9 @@
+<script>
+// Redirects users to the Home route.
+// NOTE: Currently using JS to avoid a TS error related to asyncData.
+export default {
+  asyncData({ redirect }) {
+    return redirect('/');
+  }
+};
+</script>
